d46e89689e2c649d6aaf9a3c608158e9396e9dc3
Konstantin Sandanov <kostyansandanov@gmail.com>
2025-05-21 02:52:10 +0800

    fix: исправлены ad_sender и ad_receiver

--- ads/models.py
+++ ads/models.py
@@ -1,9 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-#П·	ExchangeProposal: Модель для предложений обмена (id, ad_sender, ad_receiver, comment, status, created_at).
-
-
 class Ad(models.Model):
     """Модель объявлений"""
     class Condition(models.TextChoices):
@@ -11,16 +8,15 @@ class Ad(models.Model):
         NEW = "new", "Новое"
 
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    title = models.CharField(max_length=150, blank=False)
+    title = models.CharField(max_length=150)
     description = models.TextField()
-    image_url = models.ImageField(upload_to='ads_image/')
+    image = models.ImageField(upload_to='ads_image/')
     category = models.CharField(max_length=150)
     condition = models.CharField(max_length=20, choices=Condition.choices, default=Condition.NEW)
-    created_at = models.DateTimeField(auto_now_add=True, null=True, blank=True)
-
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"{self.title}|{self.category}"
+        return f"{self.title} | {self.category}"
 
 
 class ExchangeProposal(models.Model):
@@ -30,12 +26,12 @@ class ExchangeProposal(models.Model):
         REJECTED = "rejected", "отклонено"
         WAITING = "waiting", "ожидание"
 
-    ad_sender_id = models.ForeignKey("Ad", on_delete=models.CASCADE, related_name="ad_sender")
-    ad_receiver_id = models.ForeignKey("Ad",on_delete=models.CASCADE, related_name="ad_receiver")
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    ad_sender = models.ForeignKey(Ad, on_delete=models.CASCADE, related_name="sent_proposals")
+    ad_receiver = models.ForeignKey(Ad, on_delete=models.CASCADE, related_name="received_proposals")
     comment = models.CharField(max_length=200, blank=True)
     status = models.CharField(max_length=20, choices=Status.choices, default=Status.WAITING)
-    created_at = models.DateTimeField(auto_now_add=True, null=True, blank=True)
-
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"{self.ad_sender_id} => {self.ad_receiver_id}"
+        return f"{self.ad_sender} => {self.ad_receiver}"
