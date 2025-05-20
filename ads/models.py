8ced47981207cfc6ce9c014ec225f8855f6c0851
Konstantin Sandanov <kostyansandanov@gmail.com>
2025-05-21 01:38:15 +0800

    fix: были добавлена related_name для модели ExchangeProposal

--- ads/models.py
+++ ads/models.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import User
 
 
 class Ad(models.Model):
+    """Модель объявлений"""
     class Condition(models.TextChoices):
         USED = "used", "б/у"
         NEW = "new", "Новое"
@@ -18,14 +19,23 @@ class Ad(models.Model):
     created_at = models.DateTimeField(auto_now_add=True, null=True, blank=True)
 
 
+    def __str__(self):
+        return f"{self.title}|{self.category}"
+
+
 class ExchangeProposal(models.Model):
+    """Модель для обмена товарами"""
     class Status(models.TextChoices):
         ACCEPTED = "accepted", "принято"
         REJECTED = "rejected", "отклонено"
         WAITING = "waiting", "ожидание"
 
-    ad_sender_id = models.ForeignKey("Ad", on_delete=models.CASCADE)
-    ad_receiver_id = models.ForeignKey("Ad",on_delete=models.CASCADE)
+    ad_sender_id = models.ForeignKey("Ad", on_delete=models.CASCADE, related_name="ad_sender")
+    ad_receiver_id = models.ForeignKey("Ad",on_delete=models.CASCADE, related_name="ad_receiver")
     comment = models.CharField(max_length=200, blank=True)
     status = models.CharField(max_length=20, choices=Status.choices, default=Status.WAITING)
     created_at = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+
+
+    def __str__(self):
+        return f"{self.ad_sender_id} => {self.ad_receiver_id}"
