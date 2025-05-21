169fc151e0f59334d65d66f3f3f3d4cdd7d8922b
Konstantin Sandanov <kostyansandanov@gmail.com>
2025-05-21 09:40:24 +0800

    fix: был удален файл test.py из-за которого создавала ошибка при тестирование, а также исправлен test_models

--- ads/tests/test_models.py
+++ ads/tests/test_models.py
@@ -5,14 +5,18 @@ from ads.models import Ad, ExchangeProposal
 @pytest.mark.django_db
 def test_ad_creation():
     user = User.objects.create_user(username="user1", password="pass")
-    ad = Ad.objects.create(user=user, title="Test", description="Desc", image_url="image.jpg", category="Категория")
+    ad = Ad.objects.create(user=user, title="Test", description="Desc", image="image.jpg", category="Категория")
     assert ad.title == "Test"
     assert ad.user == user
 
 @pytest.mark.django_db
 def test_exchange_proposal_creation():
     user = User.objects.create_user(username="user2", password="pass")
-    ad1 = Ad.objects.create(user=user, title="Ad 1", description="...", image_url="img.jpg", category="A")
-    ad2 = Ad.objects.create(user=user, title="Ad 2", description="...", image_url="img.jpg", category="B")
-    proposal = ExchangeProposal.objects.create(user=user, ad_sender_id=ad1, ad_receiver_id=ad2)
-    assert proposal.ad_sender_id == ad1
+    ad1 = Ad.objects.create(user=user, title="Ad 1", description="...", image="img.jpg", category="A")
+    ad2 = Ad.objects.create(user=user, title="Ad 2", description="...", image="img.jpg", category="B")
+    
+    proposal = ExchangeProposal.objects.create(user=user, ad_sender=ad1, ad_receiver=ad2)
+
+    assert proposal.user == user
+    assert proposal.ad_sender == ad1
+    assert proposal.ad_receiver == ad2
